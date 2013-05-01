58078b6e0cbb7f9b06dc7f192278b9d6c6b1ffdb
Remy Marquis <remy.marquis@gmail.com>
2013-05-01 10:06:06 +0200

    general: fixed mp_bin dependency removal, refs #299
    Count of Local official pak.pk3 was buggy. It worked when mp_bin.pk3
    was also counted in, but that was purely incidental.

--- src/sys/sys_main.c
+++ src/sys/sys_main.c
@@ -601,7 +601,7 @@ void *Sys_LoadDll(const char *name,
 	gamedir  = Cvar_VariableString("fs_game");
 
 #ifndef DEDICATED
-	// if the server is pure, extract the dlls from the mp_bin.pk3 so
+	// if the server is pure, extract the dlls from the mod_bin.pk3 so
 	// that they can be referenced
 	if (Cvar_VariableValue("sv_pure") && Q_stricmp(name, "qagame"))
 	{
