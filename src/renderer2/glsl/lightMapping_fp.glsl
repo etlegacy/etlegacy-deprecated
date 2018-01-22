b837878219c6a8627df9826d697bdc2b45c032e5
twt_thunder <thunder@thewolfteam.org>
2018-01-22 19:23:10 +0100

    get rid of diddelidot and get in a specular that works
    -removed lightmap intensity
    -removed NdotL
    -removed NdotLnobump
    -using lightmapcolor instead of deluxemapcolor
    -new specular term

--- src/renderer2/glsl/lightMapping_fp.glsl
+++ src/renderer2/glsl/lightMapping_fp.glsl
@@ -25,7 +25,7 @@ varying vec3 var_Normal;
 
 varying vec4 var_Color;
 
-#define LIGTMAP_INTENSITY 0.75
+//#define LIGTMAP_INTENSITY 0.75
 
 void main()
 {
@@ -33,7 +33,7 @@ void main()
 	vec4 deluxemapColor = texture2D(u_DeluxeMap, var_TexLight);
 
 	//lower the lightmap intensity (this should be done on load)
-	lightmapColor.rgb = pow(lightmapColor.rgb, vec3(1.0 / LIGTMAP_INTENSITY));
+	//lightmapColor.rgb = pow(lightmapColor.rgb, vec3(1.0 / LIGTMAP_INTENSITY));
 
 #if defined(USE_PORTAL_CLIPPING)
 	float dist = dot(var_Position.xyz, u_PortalPlane.xyz) - u_PortalPlane.w;
@@ -78,21 +78,9 @@ void main()
 
 	// size and start position of search in texture space
 	vec2 S = V.xy * -u_DepthScale / V.z;
-
-#if 0
-	vec2 texOffset = vec2(0.0);
-	for (int i = 0; i < 4; i++)
-	{
-		vec4  Normal = texture2D(u_NormalMap, texNormal.st + texOffset);
-		float height = Normal.a * 0.2 - 0.0125;
-		texOffset += height * Normal.z * S;
-	}
-#else
-	float depth = RayIntersectDisplaceMap(texNormal, S, u_NormalMap);
-
+    float depth = RayIntersectDisplaceMap(texNormal, S, u_NormalMap);
 	// compute texcoords offset
 	vec2 texOffset = S * depth;
-#endif
 
 	texDiffuse.st  += texOffset;
 	texNormal.st   += texOffset;
@@ -122,43 +110,30 @@ void main()
 
 
 	// compute normal in world space from normalmap
-	vec3 N = (2.0 * (texture2D(u_NormalMap, texNormal).xyz - 0.5));
-	//N.x = -N.x;
-	//N = normalize(N);
-	//N = normalize(var_Normal.xyz);
-	N = normalize(tangentToWorldMatrix * N);
+	vec3 N =normalize(2.0 * (texture2D(u_NormalMap, texNormal).xyz - 0.5));
+	
+	
 
 	// compute light direction in world space
-	vec3 L = 2.0 * (deluxemapColor.xyz - 0.5);
+	vec3 L =normalize(2.0 * (lightmapColor.xyz - 0.5));
 	//L = normalize(L);
 
 	// compute half angle in world space
 	vec3 H = normalize(L + I);
 
+	//diffuse lightining
+	float NL = clamp(dot(N, L), 0.0, 1.0);
 	// compute light color from world space lightmap
-	vec3 lightColor = lightmapColor.rgb;
-
-	// compute the specular term
-	vec3 specular = texture2D(u_SpecularMap, texSpecular).rgb;
-
-	float NdotL = clamp(dot(N, L), 0.0, 1.0);
-
-	float NdotLnobump = clamp(dot(normalize(var_Normal.xyz), L), 0.004, 1.0);
-	//vec3 lightColorNoNdotL = clamp(lightColor.rgb / NdotLnobump, 0.0, 1.0);
-
-	//float NdotLnobump = dot(normalize(var_Normal.xyz), L);
-	vec3 lightColorNoNdotL = lightColor.rgb / NdotLnobump;
-
+	vec3 lightColor = lightmapColor.rgb * NL;
+	
+	// compute the specular term in worldspace
+	vec3 specular = texture2D(u_SpecularMap, texSpecular).rgb * lightColor.rgb * pow(clamp(dot(N, H), 0.0, 1.0), r_SpecularExponent) * r_SpecularScale;
+	
+	
 	// compute final color
 	vec4 color = diffuse;
-	// color = vec4(vec3(1.0, 1.0, 1.0), diffuse.a);
-	//color.rgb = vec3(NdotLnobump, NdotLnobump, NdotLnobump);
-	//color.rgb *= lightColor.rgb;
-	//color.rgb = lightColorNoNdotL.rgb * NdotL;
-	color.rgb *= clamp(lightColorNoNdotL.rgb * NdotL, lightColor.rgb * 0.3, lightColor.rgb);
-	//color.rgb *= diffuse.rgb;
-	//color.rgb = L * 0.5 + 0.5;
-	color.rgb += specular * lightColorNoNdotL * pow(clamp(dot(N, H), 0.0, 1.0), r_SpecularExponent) * r_SpecularScale;
+	color.rgb *= lightColor.rgb;
+	color.rgb += specular;
 	// for smooth terrain blending
 	color.a   *= var_Color.a; 
 
