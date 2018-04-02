26c8770dee24f0394eef95b7d8e859565e4e900e
twt_thunder <thunder@thewolfteam.org>
2018-04-02 10:10:11 +0200

    fix a bit and cleanup vertexrenders

--- src/renderer2/glsl/vertexLighting_DBS_entity_fp.glsl
+++ src/renderer2/glsl/vertexLighting_DBS_entity_fp.glsl
@@ -101,67 +101,7 @@ void main()
 	texSpecular.st += texOffset;
 #endif // end USE_PARALLAX_MAPPING
 
-	// compute normal in world space from normalmap
-	vec3 N = normalize(tangentToWorldMatrix * (2.0 * (texture2D(u_NormalMap, texNormal).xyz - 0.5)));
-
-	// compute half angle in world space
-	vec3 H = normalize(L + V);
-
-	// compute specular reflection
-	vec3 R =  reflect(-L, N); 
-
-	// compute the specular term
-#if defined(USE_REFLECTIVE_SPECULAR)
-
-	vec3 specular = texture2D(u_SpecularMap, texSpecular).rgb;
-
-	vec4 envColor0 = textureCube(u_EnvironmentMap0, reflect(-V, N)).rgba;
-	vec4 envColor1 = textureCube(u_EnvironmentMap1, reflect(-V, N)).rgba;
-
-	specular *= mix(envColor0, envColor1, u_EnvironmentInterpolation).rgb;
-
-	// Blinn-Phong
-	float NH = clamp(dot(N, H), 0, 1);
-	specular *= u_LightColor * pow(NH, r_SpecularExponent2) * r_SpecularScale;
-	// FIXME?
-	//specular *= u_LightColor * pow(max(dot(V, R), 0.0), r_SpecularExponent) * r_SpecularScale;
-
-#if 0
-	gl_FragColor = vec4(specular, 1.0);
-	// gl_FragColor = vec4(u_EnvironmentInterpolation, u_EnvironmentInterpolation, u_EnvironmentInterpolation, 1.0);
-	// gl_FragColor = envColor0;
-	return;
-#endif
-
-#else
-
-	// simple Blinn-Phong
-	float NH       = clamp(dot(N, H), 0, 1);
-	//vec3  specular = texture2D(u_SpecularMap, texSpecular).rgb * u_LightColor * pow(NH, r_SpecularExponent) * r_SpecularScale;
-	vec3  specular = texture2D(u_SpecularMap, texSpecular).rgb * u_LightColor * pow(max(dot(V, R), 0.0), r_SpecularExponent) * r_SpecularScale;
-
-#endif // end USE_REFLECTIVE_SPECULAR
-
-
-#else // else USE_NORMAL_MAPPING
-
-	vec3 N;
-
-#if defined(TWOSIDED)
-	if (gl_FrontFacing)
-	{
-		N = -normalize(var_Normal);
-		// gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-		// return;
-	}
-	else
-#endif
-	{
-		N = normalize(var_Normal);
-	}
-
-#endif // end USE_NORMAL_MAPPING
-
+	
 
 	// compute the diffuse term
 	vec4 diffuse = texture2D(u_DiffuseMap, texDiffuse);
@@ -184,17 +124,15 @@ void main()
 	}
 #endif
 
+// compute normal in world space from normalmap
+	vec3 N = normalize(tangentToWorldMatrix * (2.0 * (texture2D(u_NormalMap, texNormal).xyz - 0.5)));
 
-// add Rim Lighting to highlight the edges
-#if defined(r_rimLighting)
-	float rim      = 1.0 - clamp(dot(N, V), 0, 1);
-	vec3  emission = r_rimColor.rgb * pow(rim, r_rimExponent);
+	// compute half angle in world space
+	vec3 H = normalize(L + V);
 
-	// gl_FragColor = vec4(emission, 1.0);
-	// return;
-#endif
+	// compute specular reflection
+	vec3 R =  reflect(-L, N); 
 
-	// compute the light term
 #if defined(r_HalfLambertLighting)
 	// http://developer.valvesoftware.com/wiki/Half_Lambert
 	float NL = dot(N, L) * 0.5 + 0.5;
@@ -210,7 +148,34 @@ void main()
  	//vec3 light = (u_AmbientColor + u_LightColor + specular) * NL;
 	//clamp(light, 0.0, 1.0);
 
-    // compute final color
+   
+	// compute the specular term
+#if defined(USE_REFLECTIVE_SPECULAR)
+
+	vec3 specular = texture2D(u_SpecularMap, texSpecular).rgb;
+
+	vec4 envColor0 = textureCube(u_EnvironmentMap0, reflect(-V, N)).rgba;
+	vec4 envColor1 = textureCube(u_EnvironmentMap1, reflect(-V, N)).rgba;
+
+	specular *= mix(envColor0, envColor1, u_EnvironmentInterpolation).rgb;
+
+	// Blinn-Phong
+	float NH = clamp(dot(N, H), 0, 1);
+	specular *= u_LightColor * pow(NH, r_SpecularExponent2) * r_SpecularScale;
+	// FIXME?
+	//specular *= u_LightColor * pow(max(dot(V, R), 0.0), r_SpecularExponent) * r_SpecularScale;
+
+
+
+#else
+
+	// simple Blinn-Phong
+	float NH       = clamp(dot(N, H), 0, 1);
+	//vec3  specular = texture2D(u_SpecularMap, texSpecular).rgb * u_LightColor * pow(NH, r_SpecularExponent) * r_SpecularScale;
+	vec3  specular = texture2D(u_SpecularMap, texSpecular).rgb * u_LightColor * pow(max(dot(V, R), 0.0), r_SpecularExponent) * r_SpecularScale;
+
+#endif // end USE_REFLECTIVE_SPECULAR
+   // compute final color
     vec4 color = diffuse;
     color.rgb *= light;
 #if defined(USE_NORMAL_MAPPING)
@@ -219,18 +184,70 @@ void main()
 #if defined(r_rimLighting)
 	color.rgb += emission;
 #endif
+	 gl_FragColor = color;
+
+#else // no normal mapping
+
+	vec3 N;
+
+#if defined(TWOSIDED)
+	if (gl_FrontFacing)
+	{
+		N = -normalize(var_Normal);
+	}
+	else
+#endif
+	{
+		N = normalize(var_Normal);
+	}
+
+	// compute the diffuse term
+	vec4 diffuse = texture2D(u_DiffuseMap, var_TexDiffuse.st);
+
+#if defined(USE_ALPHA_TESTING)
+	if (u_AlphaTest == ATEST_GT_0 && diffuse.a <= 0.0)
+	{
+		discard;
+		return;
+	}
+	else if (u_AlphaTest == ATEST_LT_128 && diffuse.a >= 0.5)
+	{
+		discard;
+		return;
+	}
+	else if (u_AlphaTest == ATEST_GE_128 && diffuse.a < 0.5)
+	{
+		discard;
+		return;
+	}
+#endif
+
+	// compute the light term
+#if defined(r_HalfLambertLighting)
+	// http://developer.valvesoftware.com/wiki/Half_Lambert
+	float NL = dot(N, L) * 0.5 + 0.5;
+	NL *= NL;
+#elif defined(r_WrapAroundLighting)
+	float NL = clamp(dot(N, L) + u_LightWrapAround, 0.0, 1.0) / clamp(1.0 + u_LightWrapAround, 0.0, 1.0);
+#else
+	float NL = clamp(dot(N, L), 0.0, 1.0);
+#endif
+
+ 	vec3 light = (u_AmbientColor + u_LightColor) * NL;
+ 	
+	//clamp(light, 0.0, 1.0);
+
+    // compute final color
+    vec4 color = diffuse;
+    color.rgb *= light;
+
+	
+
+#if defined(r_rimLighting)
+	color.rgb += emission;
+#endif
 
 	gl_FragColor = color;
 
-	//gl_FragColor = vec4(vec3(NL, NL, NL), diffuse.a);
-
-#if 0
-#if defined(USE_PARALLAX_MAPPING)
-	gl_FragColor = vec4(vec3(1.0, 0.0, 0.0), diffuse.a);
-#elif defined(USE_NORMAL_MAPPING)
-	gl_FragColor = vec4(vec3(0.0, 0.0, 1.0), diffuse.a);
-#else
-	gl_FragColor = vec4(vec3(0.0, 1.0, 0.0), diffuse.a);
-#endif
-#endif
+#endif //vanilla
 }
