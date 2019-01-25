739ca80a7aafedb34f98f599924ea5ca7ac0fea6
techyian <ian.auty1@outlook.com>
2019-01-25 14:09:35 +0000

    Look for GLES header and lib in VideoCore subdirectory on RPi.

--- cmake/FindGLES.cmake
+++ cmake/FindGLES.cmake
@@ -10,5 +10,12 @@
 find_path(GLES_INCLUDE_DIR GLES/gl.h)
 find_library(GLES_LIBRARY NAMES GLESv1_CM)
 
+if(ARM AND NOT GLES_LIBRARY)
+{
+	# Try RPi location
+	find_path(GLES_INCLUDE_DIR /opt/vc/include/GLES/gl.h)
+	find_library(GLES_LIBRARY NAMES /opt/vc/lib/GLESv1_CM.so)
+}
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLES DEFAULT_MSG GLES_LIBRARY GLES_INCLUDE_DIR)
